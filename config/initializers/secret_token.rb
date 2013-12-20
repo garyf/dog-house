6560fbb9bf31310b5a930fa977a708e334bb2c00
Gary Fleshman <gfleshman@newforge-tech.com>
2013-12-20 16:31:51 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RequestRateLimit0::Application.config.secret_key_base = '20825d49e4d1a25883093c02ee59e4f8b62e646e1ee0fccf18ef8255e48fe2da4c25e27212cef6828019c55502524e082d7e0869d275a7a72b1e09b2f69fd1c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RequestRateLimit0::Application.config.session_store :cookie_store, key: '_request_rate_limit_0_session'
